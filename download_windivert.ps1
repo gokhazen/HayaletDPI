162b1bf389e6e7d29974fb668a8d51ea22143b23
Gökhan Özen <41stgg@gmail.com>
2026-04-01 21:13:49 +0300

    Update

--- download_windivert.ps1
+++
@@ -1,8 +0,0 @@
-$ProgressPreference = 'SilentlyContinue'
-$url = "https://reqrypt.org/download/WinDivert-2.2.0-D.zip"
-$zipPath = "WinDivert.zip"
-Write-Host "Downloading WinDivert..."
-Invoke-WebRequest -Uri $url -OutFile $zipPath
-Write-Host "Unzipping..."
-Expand-Archive -Path $zipPath -DestinationPath . -Force
-Write-Host "Done"
--- src/version.h
+++
@@ -1,7 +0,0 @@
-#ifndef VERSION_H
-#define VERSION_H
-
-#define HAYALET_VERSION "v0.5.1-Hayalet"
-#define USER_FILES_PATH "userfiles\\"
-
-#endif
